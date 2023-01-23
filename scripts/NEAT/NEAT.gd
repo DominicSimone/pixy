9d4d70b0b9dd47f6d78e79131cd6806ee59d84ab
Dominic Simone <8732239+DominicSimone@users.noreply.github.com>
2023-01-22 22:40:46 -0800

    continue transpile

--- scripts/NEAT/NEAT.gd
+++ scripts/NEAT/NEAT.gd
@@ -35,16 +35,101 @@ class Pool extends Resource:
 	var current_species: int = 0
 	var current_genome: int = 0
 	var max_fitness = 0
+	var config: NEATConfig
+	
+	func new_generation():
+		# BOOK 777
+		pass
+	
+	func add_to_species(child: Genome):
+		for spec in species:
+			if Genome.same_species(child, spec.genomes[0]):
+				spec.genomes.append(child)
+				return
+		
+		var child_species = Species.new()
+		child_species.genomes.append(child)
+		species.append(child_species)
+	
+	func remove_weak_species():
+		var survived: Array[Species] = []
+		
+		var sum = total_avg_fitness()
+		for spec in species:
+			var breed = floor(spec.avg_fitness / sum * config.population)
+			if breed >= 1:
+				survived.append(spec)
+		
+		species = survived
+	
+	func remove_stale_species():
+		var survived: Array[Species] = []
+		
+		for spec in species:
+			
+			spec.genomes.sort_custom(func(a,b): return a.fitness > b.fitness)
+			
+			if spec.genomes[0].fitness > spec.top_fitness:
+				spec.top_fitness = spec.genomes[0].fitness
+				spec.staleness = 0
+			else:
+				spec.staleness += 1
+			
+			if spec.staleness < config.stale_species or spec.top_fitness >= max_fitness:
+				survived.append(spec)
+		
+		species = survived
+	
+	func cull_species(cut_to_one: bool):
+		for spec in species:
+			spec.genomes.sort_custom(func(a,b): a.fitness > b.fitness)
+			
+			var remaining = ceil(spec.genomes.size() / 2.0)
+			if cut_to_one:
+				remaining = 1
+			
+			spec.genomes.resize(remaining)
+	
+	func total_avg_fitness():
+		return species.reduce(func(acc, s): acc + s.avg_fitness, 0.0)
+	
+	func rank_globally():
+		var all_genomes = []
+		for spec in species:
+			for genome in spec.genomes:
+				all_genomes.append(genome)
+		
+		all_genomes.sort_custom(func(a, b): return a.fitness < b.fitness)
+		
+		for rank in all_genomes.size():
+			all_genomes[rank].global_rank = rank
 
 	func new_innovation() -> int:
 		innovations += 1
 		return innovations
 
 class Species extends Resource:
+	var parent_pool: Pool
 	var top_fitness = 0
 	var avg_fitness = 0
 	var staleness = 0
-	var genomes: Array
+	var genomes: Array[Genome]
+	
+	func calc_avg_fitness_rank():
+		var total = genomes.reduce(func(a,b): return a + b) as float
+		avg_fitness = total / genomes.size()
+	
+	func breed_child():
+		var child: Genome
+		
+		if randf() < parent_pool.config.crossover_chance:
+			child = Genome.crossover(genomes.pick_random(), genomes.pick_random())
+		else:
+			child = genomes.pick_random().duplicate(true)
+		
+		child.mutate()
+		
+		return child
 
 class Genome extends Resource:
 	var parent_pool: Pool
@@ -56,10 +141,58 @@ class Genome extends Resource:
 	var global_rank = 0
 	var mutation_rates: MutationRates 
 	
-	# TODO rename toggle_mutate
-	func enable_disable_mutate():
-		# BOOKMARK 518
-		pass 
+	func mutate():
+		
+		for property in mutation_rates.get_property_list():
+			var rate = mutation_rates._get(property.name)
+			if randf() < 0.5:
+				mutation_rates._set(property.name, rate * 0.95)
+			else:
+				mutation_rates._set(property.name, rate * 1.05263)
+		
+		if randf() < mutation_rates.connections:
+			point_mutate()
+		
+		var p = mutation_rates.link
+		while p > 0:
+			if randf() < p:
+				link_mutate(false)
+			p -= 1
+		
+		p = mutation_rates.bias
+		while p > 0:
+			if randf() < p:
+				link_mutate(true)
+			p -= 1
+		
+		p = mutation_rates.node
+		while p > 0:
+			if randf() < p:
+				node_mutate()
+			p -= 1
+		
+		p = mutation_rates.enable
+		while p > 0:
+			if randf() < p:
+				enable_disable_mutate(true)
+			p -= 1
+		
+		p = mutation_rates.disable
+		while p > 0:
+			if randf() < p:
+				enable_disable_mutate(false)
+			p -= 1
+	
+	func enable_disable_mutate(toggle_value: bool):
+		var candidates = []
+		for gene in genes:
+			if gene.enabled != toggle_value:
+				candidates.append(gene)
+		
+		if candidates.is_empty():
+			return
+		
+		candidates.pick_random().enabled = toggle_value 
 	
 	func node_mutate():
 		if genes.is_empty():
@@ -73,14 +206,14 @@ class Genome extends Resource:
 		
 		gene.enabled = false
 		
-		var gene1 = gene.duplicate()
+		var gene1 = gene.duplicate(true)
 		gene1.out = max_neuron
 		gene1.weight = 1.0
 		gene1.innovation = parent_pool.new_innovation()
 		gene1.enabled = true
 		genes.append(gene1)
 		
-		var gene2 = gene.duplicate()
+		var gene2 = gene.duplicate(true)
 		gene2.into = max_neuron
 		gene2.innovation = parent_pool.new_innovation()
 		gene2.enabled = true
@@ -149,15 +282,51 @@ class Genome extends Resource:
 		
 		return randi_range(0, neurons.size() - 1)
 	
-	func mutate():
-		pass
-	
 	static func basic() -> Genome:
 		var genome = Genome.new()
 		# local innovation = 1
-		# genome.max_neuron = Inputs
+		# genome._neuron = Inputs
 		genome.mutate()
 		return genome
+	
+	static func same_species(first: Genome, second: Genome) -> bool:
+		var dd = first.parent_pool.config.delta_disjoint * disjoint(first, second)
+		var dw = first.parent_pool.config.delta_weights * weights(first, second)
+		return (dd + dw) < first.parent_pool.config.delta_threshold
+	
+	static func weights(first: Genome, second: Genome):
+		var second_genes = {}
+		for gene in second.genes:
+			second_genes[gene.innovation] = gene
+			
+		var sum = 0.0
+		var coincident = 0.0
+		
+		for gene in first.genes:
+			if second_genes.has(gene.innovation):
+				sum += abs(gene.weight - second_genes[gene.innovation].weight)
+				coincident += 1
+		
+		return sum / coincident
+	
+	static func disjoint(first: Genome, second: Genome) -> float:
+		var first_genes = {}
+		var second_genes = {}
+		var disjoint_genes = 0.0
+		
+		for gene in first.genes:
+			first_genes[gene.innovation] = true
+		
+		for gene in second.genes:
+			second_genes[gene.innovation] = true
+			if first_genes[gene.innovation] == false:
+				disjoint_genes += 1
+			
+		for gene in first.genes:
+			if second_genes[gene.innovation] == false:
+				disjoint_genes += 1
+		
+		return disjoint_genes / max(first_genes.size(), second_genes.size())
 	
 	static func crossover(first: Genome, second: Genome) -> Genome:
 		var child = Genome.new()
@@ -176,12 +345,12 @@ class Genome extends Resource:
 		for gene in g1.genes:
 			var gene2 = innovations2.get(gene.innovation)
 			if gene2 == null and gene2.enabled and randi_range(1, 2) == 1:
-				child.genes.append(gene2.duplicate())
+				child.genes.append(gene2.duplicate(true))
 			else:
-				child.genes.append(gene.duplicate())
+				child.genes.append(gene.duplicate(true))
 		
 		child.max_neuron = max(g1.max_neuron, g2.max_neuron)
-		child.mutation_rates = g1.mutation_rates.duplicate()
+		child.mutation_rates = g1.mutation_rates.duplicate(true)
 		
 		return child
 
@@ -200,13 +369,15 @@ class Gene extends Resource:
 	var weight = 0.0
 	var enabled: bool = true
 	var innovation: int = 0
+	
 
 class Neuron extends Resource:
 	var incoming: Array[Gene]
 	var value = 0.0
 
 class Network extends Resource:
-	var neurons: Array[Neuron] # Might need to be a Dictionary to cope with large size and null potential
+	var neurons: Array[Neuron] # Might need to be a Dictionary to cope with large max index and null values
+	# could also move to using max_nodes and max_nodes_per_network so we can limit size of array
 	var config: NEATConfig
 	var num_inputs: int
 	var num_outputs: int
@@ -214,10 +385,10 @@ class Network extends Resource:
 	# TODO make sure number of nodes present in neurons reflects the +1 from the bias node input
 	static func generate(genome: Genome, config: NEATConfig, num_inputs: int, num_outputs: int):
 		var network = Network.new()
-		network.config = config.duplicate()
+		network.config = config.duplicate(true)
 		network.num_inputs = num_inputs
 		network.num_outputs = num_outputs
-		network.neurons.resize(num_outputs + config.max_nodes)
+		network.neurons.resize(num_outputs + config.max_nodes) # ~spooky~
 		
 		for i in num_inputs:
 			network.neurons[i] = Neuron.new()
